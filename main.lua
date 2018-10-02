fbb0c61bccb3e00ba6e0b736f7465000a71d8249
arti0612 <41853420+arti0612@users.noreply.github.com>
2018-10-02 23:23:19 +0530

    completed

--- main.lua
+++ main.lua
@@ -1,3 +1,9 @@
+local widget = require("widget")
+
+local winningLine
+local winningText
+local resetButton
+local allowMoves = true
 
 local title = display.newText("Tic-Tac-Toe", display.contentCenterX, 25, native.SystemFontBold, 20)
 
@@ -11,18 +17,98 @@ local col = { 0.2, 0.2, 0.2 }
 
 local player = "X"
 
+local winPatterns = {}
+
+winPatterns[1] = { 1, 2, 3 }
+winPatterns[2] = { 4, 5, 6 }
+winPatterns[3] = { 7, 8, 9 }
+winPatterns[4] = { 1, 4, 7 }
+winPatterns[5] = { 2, 5, 8 }
+winPatterns[6] = { 3, 6, 9 }
+winPatterns[7] = { 1, 5, 9 }
+winPatterns[8] = { 7, 5, 3 }
+
+local function resetGame()
+    for i=1, #spots do
+        spots[i].moveText.text = " "
+        spots[i].moveType = nil
+    end
+    
+    display.remove(winningLine)
+    winningLine = nil
+    display.remove(winningText)
+    winningText = nil
+    display.remove(resetButton)
+    resetButton = nil
+    player = "X"
+    allowMoves = true
+end
+
+local function gameOver(winningMove, currentPlayer)
+    allowMoves = false
+    if winningMove then
+        local startX = spots[winPatterns[winningMove][1]].x
+        local startY = spots[winPatterns[winningMove][1]].y
+        local endX = spots[winPatterns[winningMove][3]].x
+        local endY = spots[winPatterns[winningMove][3]].y
+        winningLine = display.newLine(startX, startY, endX, endY)
+        winningLine:setStrokeColor(1, 0, 0)
+        winningLine.strokeWidth = 5
+        winningText = display.newText("Player " .. currentPlayer .. " won!", display.contentCenterX, display.contentHeight - 60, native.SystemFontBold, 30)
+    else
+        winningText = display.newText("Stalemate", display.contentCenterX, display.contentHeight - 60, native.SystemFontBold, 30)
+    end
+    resetButton = widget.newButton({
+        label = "Reset",
+        x = display.contentCenterX,
+        y = display.contentHeight - 20,
+        font = native.SystemFontBold,
+        fontSize = 20,
+        labelColor = {default = {0.75, 0.75, 1}, over = {1, 1, 1}},
+        onPress = resetGame
+    })
+end
+
+local function stalemate()
+    for i=1, #spots do
+        if spots[i].moveType == nil then
+            return
+        end
+    end
+
+    gameOver(nil, nil)
+end
+
+local function isGameOver(currentPlayer)
+    local isWinner = false
+    for i=1, #winPatterns do
+        if spots[winPatterns[i][1]].moveType == currentPlayer and 
+            spots[winPatterns[i][2]].moveType == currentPlayer and 
+            spots[winPatterns[i][3]].moveType == currentPlayer then
+                isWinner = true
+                gameOver( i, currentPlayer )
+            break
+        end
+    end
+
+    if not isWinner then
+        stalemate()
+    end
+
+    if currentPlayer == "O" then 
+        player = "X"
+    else
+        player = "O"
+    end
+end
+
 local function handleTouch(event)
     if event.phase == "began" then
-        if event.target.moveType == nil then
-            if player == "X" then
-                event.target.moveText.text = player
-                event.target.moveType = player
-                player = "O"
-            else
-                event.target.moveText.text = player
-                event.target.moveType = player
-                player = "X"
-            end
+        if event.target.moveType == nil and allowMoves then
+            event.target.moveText.text = player
+            event.target.moveType = player
+            
+            isGameOver(player)
         end
     end
     return true
